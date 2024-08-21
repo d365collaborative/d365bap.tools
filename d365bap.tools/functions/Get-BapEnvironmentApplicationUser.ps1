0be7287d28e91d7b1a5433e4bf3b13a69abfaa82
Mötz Jensen <motz@e-s.dk>
2024-08-21 19:13:54 +0200

    Refactor: Correct extract of the ApplicationUsers

--- d365bap.tools/functions/Get-BapEnvironmentApplicationUser.ps1
+++ d365bap.tools/functions/Get-BapEnvironmentApplicationUser.ps1
@@ -13,6 +13,11 @@
         
         This can be obtained from the Get-BapEnvironment cmdlet
         
+    .PARAMETER IncludePpacApplications
+        Instruct the cmdlet to include all PPAC applications in the output
+        
+        This will include all applications that are "hidden", but utilized by the PPAC Environment
+
     .PARAMETER AsExcelOutput
         Instruct the cmdlet to output all details directly to an Excel file
         
@@ -24,13 +29,40 @@
         This will fetch all ApplicationUsers from the environment.
         
         Sample output:
-        PpacAppId                            PpacAppName                    PpacApplicationUserId                SolutionId
-        -----                                -------                        -----------------                    ----------
-        b6e52ceb-f771-41ff-bd99-917523b28eaf AIBuilder_StructuredML_Prod_C  3bafba76-60bf-413d-a4c4-5c49ccabfb12 bf85e0c8-aa47
-        21ceaf7c-054c-43f6-8b14-ef6d04b90a21 AIBuilderProd                  560c9a6c-4535-4066-a415-480d1493cf98 bf85e0c8-aa47
-        c76313fd-5c6f-4f1f-9869-c884fa7fe226 AppDeploymentOrchestration     d88a3535-ebf0-4b2b-ad23-90e686660a64 99aee001-009e
-        29494271-7e38-4433-8bf8-06d335299a17 AriaMdlExporter                8bf8862f-5036-42b0-a4f8-1b638db7896b 99aee001-009e
+        PpacSystemUserId                     PpacApplicationName            PpacApplicationUserId                State
+        ----------------                     -------------------            ---------------------                -----
+        b6e52ceb-f771-41ff-bd99-917523b28eaf Power Apps Checker Application 3bafba76-60bf-413d-a4c4-5c49ccabfb12 Active
+        21ceaf7c-054c-43f6-8b14-ef6d04b90a21 Microsoft Forms Pro            560c9a6c-4535-4066-a415-480d1493cf98 Active
+        c76313fd-5c6f-4f1f-9869-c884fa7fe226 # PowerPlatform-essence-uat    d88a3535-ebf0-4b2b-ad23-90e686660a64 Active
+        29494271-7e38-4433-8bf8-06d335299a17 # PowerPlatform-essence-uat    8bf8862f-5036-42b0-a4f8-1b638db7896b Active
         
+    .EXAMPLE
+        PS C:\> Get-BapEnvironmentApplicationUser -EnvironmentId *test*
+        
+        This will fetch all ApplicationUsers from the environment.
+        
+        Sample output:
+        PpacSystemUserId                     PpacApplicationName            PpacApplicationUserId                State
+        ----------------                     -------------------            ---------------------                -----
+        b6e52ceb-f771-41ff-bd99-917523b28eaf Power Apps Checker Application 3bafba76-60bf-413d-a4c4-5c49ccabfb12 Active
+        21ceaf7c-054c-43f6-8b14-ef6d04b90a21 Microsoft Forms Pro            560c9a6c-4535-4066-a415-480d1493cf98 Active
+        c76313fd-5c6f-4f1f-9869-c884fa7fe226 # PowerPlatform-essence-uat    d88a3535-ebf0-4b2b-ad23-90e686660a64 Active
+        29494271-7e38-4433-8bf8-06d335299a17 # PowerPlatform-essence-uat    8bf8862f-5036-42b0-a4f8-1b638db7896b Active
+
+    .EXAMPLE
+        PS C:\> Get-BapEnvironmentApplicationUser -EnvironmentId *test* -IncludePpacApplications
+        
+        This will fetch all ApplicationUsers from the environment.
+        It will include all "hidden" PPAC applications in the output.
+
+        Sample output:
+        PpacSystemUserId                     PpacApplicationName            PpacApplicationUserId                State
+        ----------------                     -------------------            ---------------------                -----
+        b6e52ceb-f771-41ff-bd99-917523b28eaf Power Apps Checker Application 3bafba76-60bf-413d-a4c4-5c49ccabfb12 Active
+        21ceaf7c-054c-43f6-8b14-ef6d04b90a21 Microsoft Forms Pro            560c9a6c-4535-4066-a415-480d1493cf98 Active
+        d88a3535-ebf0-4b2b-ad23-90e686660a64 # URAssignment                 c76313fd-5c6f-4f1f-9869-c884fa7fe226 Active
+        8bf8862f-5036-42b0-a4f8-1b638db7896b # UnifiedRoutingForRecord_App  29494271-7e38-4433-8bf8-06d335299a17 Active
+
     .EXAMPLE
         PS C:\> Get-BapEnvironmentApplicationUser -EnvironmentId eec2c11a-a4c7-4e1d-b8ed-f62acc9c74c6 -AsExcelOutput
         
