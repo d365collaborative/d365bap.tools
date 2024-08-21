3b43bd21b3b0de698f96a9dbb5abb79b8c1affb1
Mötz Jensen <motz@e-s.dk>
2024-08-21 13:37:21 +0200

    Refactor: All internal access token has been refactored
    While extending the Security Role cmdlet

--- d365bap.tools/internal/functions/Test-Guid.ps1
+++ d365bap.tools/internal/functions/Test-Guid.ps1
@@ -6,6 +6,9 @@
     .DESCRIPTION
         Validates a given input string and checks string is a valid GUID by using the .NET method Guid.TryParse
         
+    .PARAMETER InputObject
+        The input string that should be validated
+        
     .EXAMPLE
         PS C:\> Test-Guid -InputObject "3363e9e1-00d8-45a1-9c0c-b93ee03f8c13"
         
