dd9de53dec11213e99463c079dadfc38842df416
Mötz Jensen <motz@e-s.dk>
2024-08-22 09:14:29 +0200

    Refactor: Get-BapEnvironmentSecurityRole

--- d365bap.tools/functions/Get-BapEnvironmentSecurityRole.ps1
+++ d365bap.tools/functions/Get-BapEnvironmentSecurityRole.ps1
@@ -33,10 +33,10 @@
         It will only list the Security Roles that are tied to the Environment.
         
         Sample output:
-        Id                                   Name                                     IsManaged RoleType
-        --                                   ----                                     --------- --------
-        5a8c8098-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature  True      Environment
-        1cbf96a1-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature  True      Environment
+        PpacRoleId                           Name                                     IsManaged PpacRoleType
+        ----------                           ----                                     --------- ------------
+        5a8c8098-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature… True      Environment
+        1cbf96a1-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature… True      Environment
         d364ba1c-1bfb-eb11-94f0-0022482381ee Accounts Payable Admin                   True      Environment
         
     .EXAMPLE
@@ -46,30 +46,24 @@
         It will only list the Security Roles that are tied to the Environment.
         
         Sample output:
-        Id                                   Name                                     IsManaged RoleType
-        --                                   ----                                     --------- --------
-        5a8c8098-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature  True      Environment
-        1cbf96a1-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature  True      Environment
+        PpacRoleId                           Name                                     IsManaged PpacRoleType
+        ----------                           ----                                     --------- ------------
+        5a8c8098-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature… True      Environment
+        1cbf96a1-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature… True      Environment
         d364ba1c-1bfb-eb11-94f0-0022482381ee Accounts Payable Admin                   True      Environment
         
     .EXAMPLE
-        PS C:\> Get-BapEnvironmentSecurityRole -EnvironmentId eec2c11a-a4c7-4e1d-b8ed-f62acc9c74c6 -Name "*Administrator*"
+        PS C:\> Get-BapEnvironmentSecurityRole -EnvironmentId *uat* -Name "*Administrator*"
         
         This will list all Security Roles, which matches the "*Administrator*" pattern, from the Dataverse environment.
         It will only list the Security Roles that are tied to the Environment.
         
         Sample output:
-        Id                                   Name                                     IsManaged RoleType
-        --                                   ----                                     --------- --------
-        5a8c8098-b933-eb11-a813-000d3a8e7ded (Deprecated) Marketing Realtime Feature  True      Environment
-        4758a2be-ccd8-ea11-a813-000d3a579805 App Profile Manager Administrator        True      Environment
+        PpacRoleId                           Name                                     IsManaged PpacRoleType
+        ----------                           ----                                     --------- ------------
         470a750f-d810-4ee7-a64a-ec002965c1ec Copilot for Service Administrator        True      Environment
-        5e4a9faa-b260-e611-8106-00155db8820b IoT - Administrator                      True      Environment
-        947229e9-e868-45cf-a361-5635eaf35ee2 Microsoft Copilot Administrator          True      Environment
-        f7f90019-dc14-e911-816a-000d3a069ebd Omnichannel administrator                True      Environment
-        6beb51c1-0eda-e911-a81c-000d3af75d63 Productivity tools administrator         True      Environment
         ebbb3fcb-fcd7-4bf8-9a48-7b5a9878e79e Sales Copilot Administrator              True      Environment
-        abce3b01-5697-4973-9d7d-fca48ca84445 Survey Services Administrator(Deprecat   True      Environment
+        abce3b01-5697-4973-9d7d-fca48ca84445 Survey Services Administrator(Deprecate… True      Environment
         63e389ae-bc55-ec11-8f8f-6045bd88b210 System Administrator                     True      Environment
         
     .EXAMPLE
@@ -79,8 +73,8 @@
         It will only list the Security Roles that are tied to the Environment.
         
         Sample output:
-        Id                                   Name                                     IsManaged RoleType
-        --                                   ----                                     --------- --------
+        PpacRoleId                           Name                                     IsManaged PpacRoleType
+        ----------                           ----                                     --------- ------------
         63e389ae-bc55-ec11-8f8f-6045bd88b210 System Administrator                     True      Environment
         
     .EXAMPLE
@@ -90,13 +84,13 @@
         It will only list the Security Roles that are tied to the Environment.
         
         Sample output:
-        Id                                   Name                                     IsManaged RoleType
-        --                                   ----                                     --------- --------
+        PpacRoleId                           Name                                     IsManaged PpacRoleType
+        ----------                           ----                                     --------- ------------
         0cdbad8e-72e7-406c-ae38-8c4406caea59 System Administrator                     False     BusinessUnit
         63e389ae-bc55-ec11-8f8f-6045bd88b210 System Administrator                     True      Environment
         
     .EXAMPLE
-        PS C:\> Get-BapEnvironmentSecurityRole -EnvironmentId eec2c11a-a4c7-4e1d-b8ed-f62acc9c74c6 -AsExcelOutput
+        PS C:\> Get-BapEnvironmentSecurityRole -EnvironmentId *uat* -AsExcelOutput
         
         This will list all Security Roles from the Dataverse environment.
         Will output all details into an Excel file, that will auto open on your machine.
@@ -157,10 +151,10 @@ function Get-BapEnvironmentSecurityRole {
                 if (-not ($roleObj.Name -like $Name)) { continue }
             }
                 
-            $tmp = $roleObj | Select-PSFObject -TypeName "D365Bap.Tools.Role" `
+            $tmp = $roleObj | Select-PSFObject -TypeName "D365Bap.Tools.PpacRole" `
                 -ExcludeProperty "@odata.etag" `
-                -Property "roleid as Id", *,
-            @{Name = "RoleType"; Expression = {
+                -Property "roleid as PpacRoleId", *,
+            @{Name = "PpacRoleType"; Expression = {
                     if ($null -eq $_.businessunitid._parentbusinessunitid_value) {
                         "Environment"
                     }
@@ -173,7 +167,7 @@ function Get-BapEnvironmentSecurityRole {
             if ($IncludeAll) {
                 $resCol.Add($tmp)
             }
-            elseif ($tmp.RoleType -eq "Environment") {
+            elseif ($tmp.PpacRoleType -eq "Environment") {
                 $resCol.Add($tmp)
             }
         }
