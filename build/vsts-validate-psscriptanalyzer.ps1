47f4cce639084eda1b0cc7184d803e739918686d
Mötz Jensen <motz@e-s.dk>
2026-04-30 11:45:33 +0200

    Feature: next version of unit tests

--- build/vsts-validate-psscriptanalyzer.ps1
+++ build/vsts-validate-psscriptanalyzer.ps1
@@ -4,7 +4,7 @@
     $TestInternal = $true
 )
 
-Import-Module "Pester" -MaximumVersion 4.99.99 -Force
+# Import-Module "Pester" -MaximumVersion 4.99.99 -Force
 
 & "$PSScriptRoot\..\d365bap.tools\tests\pester-PSScriptAnalyzer.ps1" `
     -TestPublic $TestPublic `
--- d365bap.tools/tests/pester-PSScriptAnalyzer.ps1
+++ d365bap.tools/tests/pester-PSScriptAnalyzer.ps1
@@ -1,14 +1,10 @@
 ﻿param (
 	$TestPublic = $true,
-	
+
 	$TestInternal = $true,
-	
-	[ValidateSet('None', 'Default', 'Passed', 'Failed', 'Pending', 'Skipped', 'Inconclusive', 'Describe', 'Context', 'Summary', 'Header', 'Fails', 'All')]
-	$Show = "None",
-	
-	$Include = "*",
-	
-	$Exclude = ""
+
+	[ValidateSet('None', 'Normal', 'Detailed', 'Diagnostic')]
+	$Output = "None"
 )
 
 Write-PSFMessage -Level Important -Message "Starting Tests"
@@ -19,6 +15,8 @@ Remove-Module d365bap.tools -ErrorAction Ignore
 Import-Module "$PSScriptRoot\..\d365bap.tools.psd1"
 Import-Module "$PSScriptRoot\..\d365bap.tools.psm1" -Force
 
+Import-Module Pester
+
 Write-PSFMessage -Level Important -Message "Creating test result folder"
 $null = New-Item -Path "$PSScriptRoot\..\.." -Name TestResults -ItemType Directory -Force
 
@@ -26,67 +24,55 @@ $totalFailed = 0
 $totalRun = 0
 
 $testresults = @()
+$file = Get-Item "$PSScriptRoot\general\PSScriptAnalyzer.Tests.ps1"
+$config = [PesterConfiguration]::Default
+$config.Run.PassThru = $true
+$config.TestResult.Enabled = $true
+$config.Output.Verbosity = $Output
 
 #region Run Public PSScriptAnalyzer Tests
 if ($TestPublic) {
-	Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
-	$file = Get-Item "$PSScriptRoot\general\PSScriptAnalyzer.Tests.ps1"
-
-	$pesterParm = @{}
-	$pesterParm.Path = $file.FullName
-	$pesterParm.Parameters = @{CommandPath = "$PSScriptRoot\..\functions" }
-
-	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-	$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-	$results = Invoke-Pester -Script $pesterParm -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c> (public functions)"
+	$config.Run.Container = New-PesterContainer -Path $file.FullName -Data @{ CommandPath = "$PSScriptRoot\..\functions" }
+	$config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).Public.xml"
+	$results = Invoke-Pester -Configuration $config
 	foreach ($result in $results) {
 		$totalRun += $result.TotalCount
 		$totalFailed += $result.FailedCount
-		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
-			$name = $_.Name
+		$result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
 			$testresults += [pscustomobject]@{
-				Describe = $_.Describe
-				Context  = $_.Context
-				Name     = "It $name"
-				Result   = $_.Result
-				Message  = $_.FailureMessage
+				Block   = $_.Block
+				Name    = "It $($_.Name)"
+				Result  = $_.Result
+				Message = $_.ErrorRecord.DisplayErrorMessage
 			}
 		}
 	}
 }
-
 #endregion Run Public PSScriptAnalyzer Tests
 
 #region Run Internal PSScriptAnalyzer Tests
 if ($TestInternal) {
-	Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
-	$file = Get-Item "$PSScriptRoot\general\PSScriptAnalyzer.Tests.ps1"
-
-	$pesterParm = @{}
-	$pesterParm.Path = $file.FullName
-	$pesterParm.Parameters = @{CommandPath = "$PSScriptRoot\..\internal\functions" }
-
-	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-	$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-	$results = Invoke-Pester -Script $pesterParm -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c> (internal functions)"
+	$config.Run.Container = New-PesterContainer -Path $file.FullName -Data @{ CommandPath = "$PSScriptRoot\..\internal\functions" }
+	$config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).Internal.xml"
+	$results = Invoke-Pester -Configuration $config
 	foreach ($result in $results) {
 		$totalRun += $result.TotalCount
 		$totalFailed += $result.FailedCount
-		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
-			$name = $_.Name
+		$result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
 			$testresults += [pscustomobject]@{
-				Describe = $_.Describe
-				Context  = $_.Context
-				Name     = "It $name"
-				Result   = $_.Result
-				Message  = $_.FailureMessage
+				Block   = $_.Block
+				Name    = "It $($_.Name)"
+				Result  = $_.Result
+				Message = $_.ErrorRecord.DisplayErrorMessage
 			}
 		}
 	}
 }
 #endregion Run Internal PSScriptAnalyzer Tests
 
-$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
+$testresults | Sort-Object Block, Name, Result, Message | Format-List
 
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
