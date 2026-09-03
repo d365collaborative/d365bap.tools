b865e3e59a5a228e8ff582c204ebef0b6e7069ca
Mötz Jensen <motz@e-s.dk>
2026-09-03 10:46:03 +0200

    Feature: Add optional Id filter to Get-PpacD365OperationHistory

--- d365bap.tools/tests/functions/Get-PpacD365OperationHistory.Tests.ps1
+++ d365bap.tools/tests/functions/Get-PpacD365OperationHistory.Tests.ps1
@@ -76,12 +76,25 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter Id' {
+			$parameter = (Get-Command Get-PpacD365OperationHistory).Parameters['Id']
+			$parameter.Name | Should -Be 'Id'
+			$parameter.ParameterType.ToString() | Should -Be System.String
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 2
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -EnvironmentId
-		__AllParameterSets -EnvironmentId -LatestOnly -AsExcelOutput -DownloadLog -DownloadPath
+		__AllParameterSets -EnvironmentId -LatestOnly -AsExcelOutput -DownloadLog -DownloadPath -Id
 		#>
 	}
 
